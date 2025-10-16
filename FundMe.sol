d493ba2057548fa47a3e4f5a021a2a03f7436637
Filip <filipcondac@gmail.com>
2025-10-16 13:46:53 +0100

    Implement fund withdrawing

---
+++ FundMe.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {PriceConverter} from "./PriceConverter.sol";
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    uint256 public minimumUsd = 5;
+
+    address[] public funders;
+    mapping(address => uint256) public addressToAmmountFunded;
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate() > minimumUsd,
+            "didn't send enoug eth"
+        );
+        funders.push(msg.sender);
+        addressToAmmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Must be owner");
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAmmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+}
